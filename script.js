6eb0b7d2e06762fc4bb5dd07b4b5f498b9a4b14c
MunasibKhan1 <mk25@students.kiron.ngo>
2021-07-04 08:58:21 +0000

    updated index.html, style.css and script.js

--- script.js
+++ script.js
@@ -1,18 +1,63 @@
 document.addEventListener("DOMContentLoaded", function(){
     
-    document.querySelector(".home").addEventListener("click", () => {
-      alert(`You clicked on ${event.target.textContent}`)
-    })
+    document.querySelector(".home").addEventListener("click", show_home)
     
-    document.querySelector(".about").addEventListener("click", function(){
-      alert(`You clicked on ${event.target.textContent}`)
-    })
+    document.querySelector(".about").addEventListener("click", show_about)
     
-    document.querySelector(".projects").addEventListener("click", function(){
-      alert(`You clicked on ${event.target.textContent}`)
-    })
+    document.querySelector(".projects").addEventListener("click", show_projects)
     
-    document.querySelector(".blog").addEventListener("click", function(){
-      alert(`You clicked on ${event.target.textContent}`)
-    })
+    document.querySelector(".blog").addEventListener("click", show_blog)
   })
+  
+  
+function show_about(){
+    const parent = document.querySelector(".container")
+    while (parent.firstChild) {
+        parent.firstChild.remove()
+    }
+    const center = document.createElement("center")
+    const p = document.createElement("p")
+    p.textContent = "Hello Welcome to about page"
+    center.appendChild(p)
+    parent.appendChild(center)
+    
+}
+
+function show_home(){
+    const parent = document.querySelector(".container")
+    while (parent.firstChild) {
+        parent.firstChild.remove()
+    }
+    const center = document.createElement("center")
+    const p = document.createElement("p")
+    p.textContent = "Hello Welcome to Home page"
+    center.appendChild(p)
+    parent.appendChild(center)
+    
+}
+
+function show_projects(){
+    const parent = document.querySelector(".container")
+    while (parent.firstChild) {
+        parent.firstChild.remove()
+    }
+    const center = document.createElement("center")
+    const p = document.createElement("p")
+    p.textContent = "Hello Welcome to Projects page"
+    center.appendChild(p)
+    parent.appendChild(center)
+    
+}
+
+function show_blog(){
+    const parent = document.querySelector(".container")
+    while (parent.firstChild) {
+        parent.firstChild.remove()
+    }
+    const center = document.createElement("center")
+    const p = document.createElement("p")
+    p.textContent = "Hello Welcome to Blog page"
+    center.appendChild(p)
+    parent.appendChild(center)
+    
+}
